c709779fb5ad77f6a4ee9294f3f1823a10b20c5e
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-10 15:04:54 +0200

    feat: create static header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,137 @@
-<footer class="my-5 text-center">
-    <p>by UdU</p>
-</footer>
+ <!-- parte alta del merchandise -->
+ <div class="up">
+
+    <div class="container">
+
+        <div class="single-element" key="index">
+            <!-- icona del link -->
+            <div class="imgagine">
+                <img src="" alt="page.title">
+            </div>
+
+            <!-- link -->
+            <a href="#">Digital comics</a>
+        </div>
+
+
+
+    </div>
+
+</div>
+
+<!-- link utili -->
+<div class="link-utili">
+
+    <div class="container">
+
+        <!-- parte dei link -->
+        <div class="links">
+
+            <!-- colonna -->
+            <div class="col-link">
+                <ul>
+                    <!-- titolo -->
+                    <li class="title">
+                        DC COMICS
+                    </li>
+
+                    <!-- link -->
+                    <li >
+                        <a href="link.url">Characters</a>
+                    </li>
+
+
+                    <!-- titolo -->
+                    <li class="title">
+                       SHOP
+                    </li>
+
+                    <!-- link -->
+                    <li>
+                        <a href="link.url">Shop DC</a>
+                    </li>
+
+                </ul>
+            </div>
+
+            <!-- colonna -->
+            <div class="col-link">
+                <ul>
+                    <!-- titolo -->
+                    <li class="title">
+                        DC
+                    </li>
+
+                    <!-- link -->
+                    <li>
+                        <a href="link.url">Terms of use</a>
+                    </li>
+
+                </ul>
+            </div>
+
+            <!-- colonna -->
+            <div class="col-link">
+                <ul>
+                    <!-- titolo -->
+                    <li class="title">
+                        SITES
+                    </li>
+
+                    <!-- link -->
+                    <li>
+                        <a href="link.url">DC</a>
+                    </li>
+
+                </ul>
+            </div>
+
+
+        </div>
+
+        <!-- parte del logo -->
+         <div class="log-bg">
+            <!-- <img src="../assets/img/dc-logo-bg.png" alt=""> -->
+         </div>
+
+    </div>
+
+</div>
+
+
+<!-- sing-up e follow us -->
+<div class="down">
+
+    <div class="container">
+
+        <button><a href="">SING-UP NOW!</a></button>
+
+        <div class="follow">
+            <span>FOLLOW US</span>
+
+            <a href="#">
+                <img src="{{Vite::asset('resources/img/footer-facebook.png') }}" alt="icona facebook">
+            </a>
+
+            <a href="#">
+                <img src="{{Vite::asset('resources/img/footer-twitter.png') }}" alt="icona facebook">
+
+            </a>
+
+            <a href="#">
+                <img src="{{Vite::asset('resources/img/footer-youtube.png') }}" alt="icona facebook">
+            </a>
+
+            <a href="#">
+                <img src="{{Vite::asset('resources/img/footer-pinterest.png') }}" alt="icona facebook">
+            </a>
+
+            <a href="#">
+
+                <img src="{{Vite::asset('resources/img/footer-periscope.png') }}" alt="icona facebook">
+            </a>
+        </div>
+
+    </div>
+
+</div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,47 @@
+<header>
+
+    <div class="container">
+        <!-- logo -->
+        <div class="logo">
+            <img src="/img/dc-logo.png" alt="Logo DC">
+        </div>
+
+        <!-- menu navigazione -->
+        <nav>
+            <ul>
+                <li>
+                    <a href="link.url" >CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="link.url" class="active">COMICS</a>
+                </li>
+                <li>
+                    <a href="link.url" >MOVIES</a>
+                </li>
+                <li>
+                    <a href="link.url" >TV</a>
+                </li>
+                <li>
+                    <a href="link.url">GAMES</a>
+                </li>
+                <li>
+                    <a href="link.url" >COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="link.url" >VIDEOS</a>
+                </li>
+                <li>
+                    <a href="link.url" >FANS</a>
+                </li>
+                <li>
+                    <a href="link.url" >NEWS</a>
+                </li>
+                <li>
+                    <a href="link.url" >SHOP</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+</header>
+
